da890e8aeee735d4b5ab7e64d8d81a41496f0565
Enchan <enchant0130@gmail.com>
2024-08-18 17:00:36 +0900

    [Update] configure project
    - maven_group, MOD IDを変更
    - Fabricのプロパティを更新
    - ソースファイルのパスを変更

--- src/client/java/me/enchan/ExampleModClient.java
+++ src/client/java/me/enchan/ExampleModClient.java
@@ -1,10 +1,11 @@
-package com.example;
+package me.enchan;
 
 import net.fabricmc.api.ClientModInitializer;
 
 public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		// This entrypoint is suitable for setting up client-specific logic, such as
+		// rendering.
 	}
 }
--- src/main/java/me/enchan/mixin/ExampleMixin.java
+++ src/main/java/me/enchan/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package me.enchan.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
